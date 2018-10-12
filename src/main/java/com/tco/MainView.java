0ad5bc367182510938164d0489e7f696f7dd45f1
Chsldz <36513140+Chsldz@users.noreply.github.com>
2018-10-12 15:53:50 +0200

    Created Vaadion10 Project

---
+++ src/main/java/com/tco/MainView.java
@@ -0,0 +1,22 @@
+package com.tco;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+/**
+ * The main view contains a button and a click listener.
+ */
+@Route("")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        Label label = new Label("Hier steht was");
+        Button button = new Button("Click me",
+                event -> Notification.show("Clicked!"));
+        add(label);
+        add(button);
+    }
+}
