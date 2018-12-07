4c57d7444c12b58e935825587ad133df6ce4a000
Chsldz <36513140+Chsldz@users.noreply.github.com>
2018-12-07 13:19:40 +0100

    MariaDB working now over Hibernate, have a look in the config for details

--- src/main/java/com/tco/MainView.java
+++ src/main/java/com/tco/MainView.java
@@ -7,18 +7,31 @@ import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 /**
  * The main view contains a button and a click listener.
  */
 @Route("main")
 public class MainView extends VerticalLayout {
+        private static SessionFactory factory;
 
     public MainView() {
         HeaderView headerView = new HeaderView();
         DashView dashView = new DashView();
         add(headerView);
         add(dashView);
+
+
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        factory = cfg.buildSessionFactory();
+
+        //ManageAuto MA = new ManageAuto();
+
+        // Hinzufügen einiger Autos
+        //Integer autoID1 = MA.addAuto("Fiat", "Punto", 50, "grün");
 
     }
 }
