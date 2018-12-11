9c1947ddfc48cf6db088792593c1d9c6aa1d1b50
Chsldz <36513140+Chsldz@users.noreply.github.com>
2018-12-11 21:11:06 +0100

    TEN-69 - Write Hibernatemanager Cleanup

--- src/main/java/com/tco/view/DashView.java
+++ src/main/java/com/tco/view/DashView.java
@@ -1,8 +1,8 @@
 package com.tco.view;
 
-import com.tco.model.HibernateManager;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 public class DashView extends VerticalLayout {
@@ -10,8 +10,7 @@ public class DashView extends VerticalLayout {
         Label label = new Label("Hier steht was");
         Button button = new Button("Click me",
                 event -> {
-            HibernateManager hibernate = new HibernateManager();
-            hibernate.method();
+                    Notification.show("Nothing happens");
                 });
 
         add(label);
